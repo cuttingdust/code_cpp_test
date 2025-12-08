5b9d753b0e8894f1f7e49b05dbc3ccecb4bbf45d
handabao <18862963156@163.com>
2025-12-08 16:41:12 +0800

    move 测试 这样会崩溃 二次删除

---
+++ src/MoveTest/src/main.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+
+class Object
+{
+public:
+    Object() : m_ptr(new int(10))
+    {
+        std::cout << "Object created\n";
+    }
+    ~Object()
+    {
+        delete m_ptr;
+        std::cout << "Object destroyed\n";
+    }
+
+private:
+    int *m_ptr = nullptr;
+};
+
+Object get_object(bool flag)
+{
+    Object A;
+    Object B;
+    if (flag)
+    {
+        return A;
+    }
+    else
+    {
+        return B;
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    Object myObj = get_object(true);
+
+    std::cout << "hello world" << std::endl;
+
+    return 0;
+}
