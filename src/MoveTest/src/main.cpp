1b3f11cccece8e4b02f62eaebd8d567252d80500
handabao <18862963156@163.com>
2025-12-08 16:54:27 +0800

    右值引用 解决浅拷贝

--- src/MoveTest/src/main.cpp
+++ src/MoveTest/src/main.cpp
@@ -12,6 +12,39 @@ public:
         delete m_ptr;
         std::cout << "Object destroyed\n";
     }
+    /// 方法1 深拷贝构造函数
+    Object(const Object &other) : m_ptr(new int(*(other.m_ptr)))
+    {
+        std::cout << "Object copied\n";
+    } /// 效率低但安全的深拷贝
+
+    Object &operator=(const Object &other)
+    {
+        if (this != &other)
+        {
+            *m_ptr = *(other.m_ptr); /// 复制值，而非指针
+        }
+        return *this;
+    }
+
+    /// 方法2 移动构造函数
+    Object(Object &&other) noexcept : m_ptr(other.m_ptr)
+    {
+        other.m_ptr = nullptr; /// *** 避免双重删除
+        std::cout << "Object moved\n";
+    } /// 移动构造函数
+
+    Object &operator=(Object &&other) noexcept
+    {
+        /// std::move赋值运算符
+        if (this != &other)
+        {
+            delete m_ptr;
+            m_ptr       = other.m_ptr;
+            other.m_ptr = nullptr;
+        }
+        return *this;
+    }
 
 private:
     int *m_ptr = nullptr;
@@ -33,6 +66,7 @@ Object get_object(bool flag)
 
 int main(int argc, char *argv[])
 {
+    /// 多次删除 /// 默认浅拷贝
     Object myObj = get_object(true);
 
     std::cout << "hello world" << std::endl;
