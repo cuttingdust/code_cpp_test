ae3b6c569847fbdc17461b1d5a5076d97e040b6a
handabao <18862963156@163.com>
2025-12-10 11:09:47 +0800

    base16编码 封装一下

---
+++ src/Base16API/include/base16.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include <string>
+#include <vector>
+
+////////////////////////////////////
+/// \brief base16编码
+/// \param data 需要编码的二进制数据
+/// \return 返回base16编码的字符串
+std::string Base16Encode(const std::vector<unsigned char> &data);
+
+
+//////////////////////////////////////
+/// \brief base16解码
+/// \param str 需要解码的base16字符串，必须二的倍数
+/// \return 返回原始的二进制数据
+std::vector<unsigned char> Base16Decode(const std::string &str);
