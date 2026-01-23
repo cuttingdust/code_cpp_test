d01cee0dfcd08d0882c9dfcb5170ea58c5287cf5
handabao <18862963156@163.com>
2026-01-23 11:25:36 +0800

    使用 submodules 三方库

--- src/update.bat
+++ src/update.bat
@@ -1,4 +1,5 @@
 @echo off
 chcp 65001 > nul
+git submodule update --remote --recursive
 cmake -S . -B build 
 timeout 2
--- src/vcpkg_install.bat
+++ src/vcpkg_install.bat
@@ -1,4 +1,7 @@
 @echo off
 chcp 65001 > nul
+git submodule add https://github.com/nlohmann/json.git third_party/json
+git submodule add https://github.com/AmokHuginnsson/replxx.git third_party/replxx
+git submodule add https://github.com/p-ranav/indicators.git third_party/indicators
 vcpkg install
 timeout 5
