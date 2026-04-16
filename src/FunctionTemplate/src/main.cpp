58d8d6f69cca099695fdc25d48595b51da483c58
handabao <18862963156@163.com>
2026-04-17 00:48:28 +0800

    模板的非类型参数

---
+++ src/FunctionTemplate/src/main.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+template <typename T, int SIZE>
+void sort(T (&arr)[SIZE])
+{
+    for (int i = 0; i < SIZE - 1; ++i)
+    {
+        for (int j = 0; j < SIZE - i - 1; ++j)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                std::swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+int main()
+{
+    int arr[] = { 5, 4, 3, 2, 1 }; // 改成逆序测试
+    sort(arr);                     // ✅ 正常工作
+
+    for (int v : arr)
+        std::cout << v << " "; // 输出: 1 2 3 4 5
+    return 0;
+}
